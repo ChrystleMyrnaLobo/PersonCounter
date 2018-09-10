7ed26c7d93bcb4b9e1a45f55e2453ff6761afc18
Chrystle <chrystle_myrna@yahoo.co.in>
2018-09-10 15:46:34 +0530

    Clean up util

--- pc_utils.py
+++ pc_utils.py
@@ -7,80 +7,6 @@ from object_detection.utils import np_box_mask_list
 from object_detection.utils import np_box_mask_list_ops
 from object_detection.utils import per_image_evaluation
 
-
-def pc_non_max_suppression(boxlist,
-                        max_output_size=10000,
-                        iou_threshold=1.0,
-                        score_threshold=-10.0):
-  """Non maximum suppression.
-
-  This op greedily selects a subset of detection bounding boxes, pruning
-  away boxes that have high IOU (intersection over union) overlap (> thresh)
-  with already selected boxes. In each iteration, the detected bounding box with
-  highest score in the available pool is selected.
-
-  Args:
-    boxlist: BoxList holding N boxes.  Must contain a 'scores' field
-      representing detection scores. All scores belong to the same class.
-    max_output_size: maximum number of retained boxes
-    iou_threshold: intersection over union threshold.
-    score_threshold: minimum score threshold. Remove the boxes with scores
-                     less than this value. Default value is set to -10. A very
-                     low threshold to pass pretty much all the boxes, unless
-                     the user sets a different score threshold.
-
-  Returns:
-    a BoxList holding M boxes where M <= max_output_size
-  Raises:
-    ValueError: if 'scores' field does not exist
-    ValueError: if threshold is not in [0, 1]
-    ValueError: if max_output_size < 0
-  """
-  if not boxlist.has_field('scores'):
-    raise ValueError('Field scores does not exist')
-  if iou_threshold < 0. or iou_threshold > 1.0:
-    raise ValueError('IOU threshold must be in [0, 1]')
-  if max_output_size < 0:
-    raise ValueError('max_output_size must be bigger than 0.')
-
-  boxlist = filter_scores_greater_than(boxlist, score_threshold)
-  if boxlist.num_boxes() == 0:
-    return boxlist
-
-  boxlist = sort_by_field(boxlist, 'scores')
-
-  # Prevent further computation if NMS is disabled.
-  if iou_threshold == 1.0:
-    if boxlist.num_boxes() > max_output_size:
-      selected_indices = np.arange(max_output_size)
-      return gather(boxlist, selected_indices)
-    else:
-      return boxlist
-
-  boxes = boxlist.get()
-  num_boxes = boxlist.num_boxes()
-  # is_index_valid is True only for all remaining valid boxes,
-  is_index_valid = np.full(num_boxes, 1, dtype=bool)
-  selected_indices = []
-  num_output = 0
-  for i in range(num_boxes):
-    if num_output < max_output_size:
-      if is_index_valid[i]:
-        num_output += 1
-        selected_indices.append(i)
-        is_index_valid[i] = False
-        valid_indices = np.where(is_index_valid)[0]
-        if valid_indices.size == 0:
-          break
-
-        intersect_over_union = np_box_ops.iou(
-            np.expand_dims(boxes[i, :], axis=0), boxes[valid_indices, :])
-        intersect_over_union = np.squeeze(intersect_over_union, axis=0)
-        is_index_valid[valid_indices] = np.logical_and(
-            is_index_valid[valid_indices],
-            intersect_over_union <= iou_threshold)
-  return gather(boxlist, np.array(selected_indices))
-
 # Override
 class pc_PerImageEvaluation(per_image_evaluation.PerImageEvaluation):
     """Evaluate detection result of a single image.PerImageEvaluation"""
@@ -92,7 +18,7 @@ class pc_PerImageEvaluation(per_image_evaluation.PerImageEvaluation):
       detected_boxes,
       detected_scores,
       groundtruth_boxes,
-      groundtruth_is_group_of_list):
+      groundtruth_is_group_of_list=None):
     """Computes overlaps and scores between detected and groudntruth boxes.
 
     Args:
@@ -120,12 +46,13 @@ class pc_PerImageEvaluation(per_image_evaluation.PerImageEvaluation):
     detected_boxlist = np_box_list_ops.pc_non_max_suppression(detected_boxlist, self.nms_max_output_boxes, self.nms_iou_threshold)
 # Sorted - need to get the updated version from here
     gt_non_group_of_boxlist = np_box_list.BoxList(groundtruth_boxes[~groundtruth_is_group_of_list])
-    gt_group_of_boxlist = np_box_list.BoxList(groundtruth_boxes[groundtruth_is_group_of_list])
+    #gt_group_of_boxlist = np_box_list.BoxList(groundtruth_boxes[groundtruth_is_group_of_list])
     iou = np_box_list_ops.iou(detected_boxlist, gt_non_group_of_boxlist)
-    ioa = np.transpose(np_box_list_ops.ioa(gt_group_of_boxlist, detected_boxlist))
+    #ioa = np.transpose(np_box_list_ops.ioa(gt_group_of_boxlist, detected_boxlist))
     scores = detected_boxlist.get_field('scores')
     num_boxes = detected_boxlist.num_boxes()
-    return iou, ioa, scores, num_boxes
+#    return iou, ioa, scores, num_boxes, detected_boxlist
+    return iou, scores, num_boxes, detected_boxlist
 
 
 ########################################################################################################################################
@@ -148,22 +75,24 @@ Filter detection score < score threshold
 def matchOnIoU(pie, detected_boxes, detected_scores, groundtruth_boxes):
     # Default value false
     num_groundtruth_boxes = np.shape(groundtruth_boxes)[0]
-    groundtruth_is_group_of_list = np.zeros(num_groundtruth_boxes, dtype=bool)
+    #groundtruth_is_group_of_list = np.zeros(num_groundtruth_boxes, dtype=bool)
 
     # Compute IoU for every detection and groundtruth pair
     # Detection with score < score threshold are ignored. Rest are sorted.
-    (iou, _, scores, num_detected_boxes) = pie._get_overlaps_and_scores_box_mode(
+    (iou, scores, num_detected_boxes, detected_boxlist) = pie._get_overlaps_and_scores_box_mode(
                detected_boxes=detected_boxes,
                detected_scores=detected_scores,
                groundtruth_boxes=groundtruth_boxes,
-               groundtruth_is_group_of_list=groundtruth_is_group_of_list)
+               groundtruth_is_group_of_list=None)
     
     # If no GT value then all detection are false positive
     if groundtruth_boxes.size == 0:
         #return scores, np.zeros(num_detected_boxes, dtype=bool)
         print "No ground truth, All FP"
         return
-    
+
+    # Restore the dictionary
+
     # Is ith detection a True positive
     tp_fp_labels = np.zeros(num_detected_boxes, dtype=bool)
 
@@ -183,6 +112,7 @@ def matchOnIoU(pie, detected_boxes, detected_scores, groundtruth_boxes):
         print "Dt", scores[i], "GT", max_overlap_gt_ids[i], "", tp_fp_labels[i]
     for i in range(num_groundtruth_boxes):
         print "GT", i, bool(is_gt_box_detected[i])
-    
+    # convert detected boxlist to dict
+    # Assign ID, return dt and tp_fp_labels
 
     return scores, max_overlap_gt_ids, tp_fp_labels, is_gt_box_detected 
